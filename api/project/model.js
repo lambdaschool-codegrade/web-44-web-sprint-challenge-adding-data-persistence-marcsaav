61c8f5927179d0cd9f2824326de5e6a52be390ad
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-13 08:47:34 -0700

    resource and project models created

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,27 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const createProject = async (project) => {
+    const pro = await db('projects').insert(project)
+    const projectToGive = {
+        ...pro,
+        project_completed: false
+    }
+    return projectToGive
+}
+
+const getProjects = async () => {
+    const projects = await db('projects')
+    const projectsToGive = projects.map((pro) => {
+        let project = {
+            ...pro,
+            project_completed: pro.project_completed === 1 ? true : false
+        }
+        return project
+    })
+    return projectsToGive
+}
+
+module.exports = {
+    createProject,
+    getProjects
+}
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,14 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const createResource = (resource) => {
+    return db('resources').insert(resource).then((ids) => {return db('resources'.where('resource_id', ids[0]))})
+}
+
+const getResources = () => {
+    return db('resources')
+}
+
+module.exports = {
+    createResource,
+    getResources
+}
